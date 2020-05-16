d933b90b6054618b767aec75c26c82c293dc5d71
Lucas Teixeira <sobrinho1@gmail.com>
2020-05-16 15:19:56 -0300

    first version

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Hello");
+
+window.addEventListener("load", main);
+
+var slideRed = document.querySelector("#rangeRed");
+var slideBlue = document.querySelector("#rangeBlue");
+var slideGreen = document.querySelector("#rangeGreen");
+
+function main() {
+  applyColor();
+  slideRed.addEventListener("change", pushRed);
+  slideBlue.addEventListener("change", pushBlue);
+  slideGreen.addEventListener("change", pushGreen);
+}
+
+function applyColor() {
+  document.querySelector("#txtRed").value = slideRed.value;
+  document.querySelector("#txtBlue").value = slideBlue.value;
+  document.querySelector("#txtGreen").value = slideGreen.value;
+
+  document.querySelector(
+    "#divRGB"
+  ).style.backgroundColor = `rgb(${slideRed.value},${slideBlue.value},${slideGreen.value})`;
+}
+
+function pushRed() {
+  document.querySelector("#txtRed").value = this.value;
+  applyColor();
+}
+
+function pushBlue() {
+  document.querySelector("#txtBlue").value = this.value;
+  applyColor();
+}
+
+function pushGreen() {
+  document.querySelector("#txtGreen").value = this.value;
+  applyColor();
+}
